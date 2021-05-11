d0ca78348376a8e47baccacd324564b6aab17702
Mazlum Arslan <arslan.mzlm@gmail.com>
2021-05-11 15:55:13 +0300

    Settings for dashboard frontend

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+mix.js('resources/js/app.js', 'public/assets/js')
+    .sass('resources/sass/app.scss', 'public/assets/css')
+    .js('resources/js/admin/app.js', 'public/assets/admin/js')
+    .sass('resources/sass/app.scss', 'public/assets/admin/css')
     .sourceMaps();
