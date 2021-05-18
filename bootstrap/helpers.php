2764c5ab86c9a8120d529b9e6b5b62f080a2dad9
Mazlum Arslan <arslan.mzlm@gmail.com>
2021-05-18 09:53:29 +0300

    New helper for meta title

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,6 +1,6 @@
 <?php
 
-if (! function_exists('relative_route')) {
+if (!function_exists('relative_route')) {
     /**
      * Generate the relative path to a named route.
      *
@@ -11,5 +11,19 @@ if (! function_exists('relative_route')) {
     function relative_route($name, $parameters = [])
     {
         return app('url')->route($name, $parameters, false);
+    }
+}
+
+if (!function_exists('meta_title')) {
+    /**
+     * Generate meta title with translate
+     *
+     * @param string $title
+     * @param string $divider
+     * @return string
+     */
+    function meta_title($title, $divider = '|')
+    {
+        return trans($title) . ' ' . $divider . ' ' . env('APP_NAME');
     }
 }
