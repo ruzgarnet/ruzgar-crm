b5fb1fc3c0c8bf0b957282457f6e4e1f9213184a
Mazlum Arslan <arslan.mzlm@gmail.com>
2021-05-19 14:57:16 +0300

    Added attribute trait for phone

---
+++ app/Models/Attributes/PhoneAttribute.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Attributes;
+
+trait PhoneAttribute
+{
+    /**
+     * Return telephone number for print
+     *
+     * @return string
+     */
+    public function getTelephonePrintAttribute()
+    {
+        return preg_replace('/(\d{3})(\d{3})(\d{2})(\d{2})/', '0 $1 $2 $3 $4', $this->telephone);
+    }
+}
