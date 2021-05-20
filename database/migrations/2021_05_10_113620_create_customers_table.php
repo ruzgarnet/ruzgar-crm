9f3185c384f7b17940e0e2afd511042845c17c0b
Mazlum Arslan <arslan.mzlm@gmail.com>
2021-05-20 13:08:54 +0300

    Updated customers table

--- database/migrations/2021_05_10_113620_create_customers_table.php
+++ database/migrations/2021_05_10_113620_create_customers_table.php
@@ -16,10 +16,14 @@ class CreateCustomersTable extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('customer_no', 31)->unique();
+            $table->string('identification_number', 15)->unique();
             $table->string('first_name', 63);
             $table->string('last_name', 63);
-            $table->string('phone', 15);
+            $table->string('telephone', 15);
             $table->string('email', 31);
+            $table->unsignedTinyInteger('type');
+            $table->string('reference_code')->unique();
+            $table->string('payment_code', 255)->unique()->nullable()->default(null);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrentOnUpdate()->nullable()->default(null);
         });
@@ -27,10 +31,8 @@ class CreateCustomersTable extends Migration
         Schema::create('customer_info', function (Blueprint $table) {
             $table->id();
             $table->foreignId('customer_id');
-            $table->string('identification_number', 15)->unique();
-            $table->string('second_phone', 15)->nullable()->default(null);
-            $table->string('reference_code')->unique();
-            $table->string('payment_token', 255)->unique()->nullable()->default(null);
+            $table->string('secondary_telephone', 15)->nullable()->default(null);
+            $table->unsignedTinyInteger('gender');
             $table->date('birthday');
             $table->foreignId('city_id');
             $table->foreignId('district_id');
@@ -43,13 +45,13 @@ class CreateCustomersTable extends Migration
             	->on('customers')
             	->onUpdate('CASCADE')
             	->onDelete('CASCADE');
-            	
+
             $table->foreign('city_id')
             	->references('id')
             	->on('cities')
             	->onUpdate('CASCADE')
             	->onDelete('CASCADE');
-            	
+
             $table->foreign('district_id')
             	->references('id')
             	->on('districts')
