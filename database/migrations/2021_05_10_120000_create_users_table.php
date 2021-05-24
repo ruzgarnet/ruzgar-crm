df909de0c100e1fdae2542f4aaf2d37e1cdae8b9
Mazlum Arslan <arslan.mzlm@gmail.com>
2021-05-24 10:28:52 +0300

    Updated users migration for staff relationship

--- database/migrations/2021_05_10_120000_create_users_table.php
+++ database/migrations/2021_05_10_120000_create_users_table.php
@@ -15,13 +15,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('staff_id')->nullable()->default(null);
             $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrentOnUpdate()->nullable()->default(null);
+
+            $table->foreign('staff_id')
+            	->references('id')
+            	->on('staff')
+            	->onUpdate('CASCADE')
+            	->onDelete('CASCADE');
         });
     }
 
