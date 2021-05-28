2f23e37446970c58d0ece1f0b315f70e09323110
Engin <engin.ergen@hotmail.com>
2021-05-28 10:41:12 +0300

    payment Migration

---
+++ database/migrations/2021_05_27_174550_create_payments_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("subscription_id");
+            $table->date("date");
+            $table->unsignedDecimal("price");
+            $table->unsignedTinyInteger("status")->default(1);
+            $table->unsignedTinyInteger("type")->nullable()->default(null);
+            $table->timestamp("paid_at")->nullable()->default(null);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrentOnUpdate()->nullable()->default(null);
+
+            $table->foreign('subscription_id')
+            	->references('id')
+            	->on('subscriptions')
+            	->onUpdate('CASCADE')
+            	->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
