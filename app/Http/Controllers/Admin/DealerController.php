bc0c0c1ec1ec88ac891c91178af2e69985c238ea
Engin <engin.ergen@hotmail.com>
2021-06-02 13:16:29 +0300

    Updated Mutator namespace in controllers

--- app/Http/Controllers/Admin/DealerController.php
+++ app/Http/Controllers/Admin/DealerController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\Mutator;
+use App\Classes\Mutator;
 use App\Models\City;
 use App\Models\Dealer;
 use App\Rules\AvailableDistrict;
