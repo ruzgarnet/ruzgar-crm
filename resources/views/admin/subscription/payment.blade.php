e569b8bb7f0bc3bb744948660c29602c3dc56f7c
arslanmzlm <arslan.mzlm@gmail.com>
2021-06-03 17:15:42 +0300

    Fixed dataTable in subs payment view

--- resources/views/admin/subscription/payment.blade.php
+++ resources/views/admin/subscription/payment.blade.php
@@ -155,17 +155,13 @@
     </div>
 @endsection
 
-@push('modal')
-    @include('admin.modals.get-payment')
-@endpush
-
 @push('style')
     <link rel="stylesheet" href="/assets/admin/vendor/datatables/datatables.min.css">
-    <link rel="stylesheet" href="/assets/admin/vendor/datatables/DataTables-1.10.24\css\dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="/assets/admin/vendor/datatables/DataTables-1.10.24/css/dataTables.bootstrap4.min.css">
 @endpush
 
 @push('script')
-    <script src="/assets/admin/vendor/datatables/DataTables-1.10.24/js/jquery.DataTables.min.js"></script>
+    <script src="/assets/admin/vendor/datatables/DataTables-1.10.24/js/jquery.dataTables.min.js"></script>
     <script src="/assets/admin/vendor/datatables/DataTables-1.10.24/js/dataTables.bootstrap4.min.js"></script>
 
     <script>
@@ -173,9 +169,17 @@
             $("#dataTable").dataTable({
                 language: {
                     url: '/assets/admin/vendor/datatables/i18n/tr.json'
-                }
+                },
+                columnDefs: [{
+                    "type": "num",
+                    "targets": 0
+                }]
             });
         })
 
     </script>
+@endpush
+
+@push('modal')
+    @include('admin.modals.get-payment')
 @endpush
