6223d8a8cfc2fdc206e55153e45d67e88209ae4f
arslanmzlm <arslan.mzlm@gmail.com>
2021-06-03 11:04:56 +0300

    Fixed phone regex in mutator

--- app/Classes/Mutator.php
+++ app/Classes/Mutator.php
@@ -17,7 +17,7 @@ class Mutator
     {
         if (is_string($str) && strlen($str) >= 10) {
             $data = preg_replace('/\D/', '', $str);
-            return preg_replace('/[\90|0]?([1-9][0-9]{9})/', '$1', $data);
+            return preg_replace('/[90|0]*([1-9][0-9]{9})/', '$1', $data);
         }
 
         return $str;
