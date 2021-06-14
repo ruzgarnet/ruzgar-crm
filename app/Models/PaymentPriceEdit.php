6f66e2c8ec4ccf8b5df23433e8a7b1d650109313
Engin <engin.ergen@hotmail.com>
2021-06-14 16:20:00 +0300

    Fixed PaymentPriceEdit problem

--- app/Models/PaymentPriceEdit.php
+++ app/Models/PaymentPriceEdit.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class EditPayment extends Model
+class PaymentPriceEdit extends Model
 {
     use HasFactory;
 
