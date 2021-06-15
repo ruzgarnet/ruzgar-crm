eec089bbaf19d89cfed41a3b44ed84ac0fadd69a
arslanmzlm <arslan.mzlm@gmail.com>
2021-06-15 14:26:17 +0300

    Telegram API improved

--- app/Classes/Telegram.php
+++ app/Classes/Telegram.php
@@ -2,6 +2,8 @@
 
 namespace App\Classes;
 
+use Exception;
+
 /**
  * Telegram API
  */
@@ -53,9 +55,14 @@ class Telegram
 
     public function __construct()
     {
-        $this->url = "https://api.telegram.org/bot";
-        $this->token = env("TELEGRAM_API_TOKEN");
-        $this->url .= $this->token."/";
+        $token = env("TELEGRAM_API_TOKEN");
+        if ($token && !empty($token)) {
+            $this->url = "https://api.telegram.org/bot";
+            $this->token = env("TELEGRAM_API_TOKEN");
+            $this->url .= $this->token . "/";
+        } else {
+            throw new Exception('Token not found', 100);
+        }
     }
 
     /**
@@ -67,6 +74,9 @@ class Telegram
      */
     public function send_message($chat_id, $message)
     {
+        if (env('APP_ENV') === 'local') {
+            $chat_id = '-562316544';
+        }
         $this->request_parameters["chat_id"] = $chat_id;
         $this->request_parameters["text"] = $message;
         $this->function = "sendMessage";
@@ -75,7 +85,7 @@ class Telegram
 
     private function send()
     {
-        $this->url .= $this->function."?".http_build_query($this->request_parameters);
+        $this->url .= $this->function . "?" . http_build_query($this->request_parameters);
         return file_get_contents($this->url);
     }
 }
