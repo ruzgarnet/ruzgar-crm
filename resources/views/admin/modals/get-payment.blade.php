73b41f16bb65b30f431f9738b7b2ab7e9f096947
arslanmzlm <arslan.mzlm@gmail.com>
2021-06-19 14:10:07 +0300

    Little fix in modals

--- resources/views/admin/modals/get-payment.blade.php
+++ resources/views/admin/modals/get-payment.blade.php
@@ -4,7 +4,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">@lang('titles.get_payment')</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="@lang('titles.close')">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
