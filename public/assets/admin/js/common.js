c8eb2cf87a51bf2f5e22cfc34bc7b76afe477120
Mazlum Arslan <arslan.mzlm@gmail.com>
2021-07-24 17:13:21 +0300

    Prevented enter key submitting

--- public/assets/admin/js/common.js
+++ public/assets/admin/js/common.js
@@ -13,6 +13,15 @@ $(function () {
         event.preventDefault();
     });
 
+    // Prevent enter submit
+    $(document).on("keypress", 'form', function (e) {
+        var code = e.keyCode || e.which;
+        if (code == 13) {
+            e.preventDefault();
+            return false;
+        }
+    });
+
     /**
      * Ajax form requests
      */
